35c180029d16a0b77aed9172b306f93f9e363ff7
Chris Wright <chris@arcane.ws>
2017-01-11 13:06:47 -0500

    Added store and reducer boilerplate

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import store from './stores/configureStore';
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store()}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import cart from './cart';
+import inventory from './inventory';
+
+export default combineReducers({
+    cart,
+    inventory
+});
