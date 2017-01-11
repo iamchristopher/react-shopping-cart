889b63aad27ae372dfce0b89042db04a24d0d530
Chris Wright <chris@arcane.ws>
2017-01-11 18:43:46 -0500

    Added sample inventory data

--- src/reducers/inventory.js
+++ src/reducers/inventory.js
@@ -1,4 +1,7 @@
 const initialState = {
+    1: { name: 'Item 1', quantityOnHand: 1 },
+    2: { name: 'Item 2', quantityOnHand: 10 },
+    3: { name: 'Item 3', quantityOnHand: 12 }
 };
 
 export default (state = initialState, action = {}) => {
