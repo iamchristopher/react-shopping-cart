56bc012112f7970299625669d76e582e5dd5bf7a
Chris Wright <chris@arcane.ws>
2017-01-12 12:57:36 -0500

    Added zero state for inventory

--- src/components/Inventory/index.js
+++ src/components/Inventory/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+    Alert,
     Button,
     Table
 } from 'react-bootstrap';
@@ -11,55 +12,67 @@ export default ({
     products,
     removeItem
 }) => (
-    <Table
-        bordered
-        hover
-        striped
-        verticalAlign="middle"
-    >
-        <tbody>
-            {products.map((product, i) => (
-                <tr key={i}>
-                    <td>
-                        {product.id}
-                    </td>
-                    <td
-                        width="67%"
-                    >
-                        {product.name}
-                    </td>
-                    <td>
-                        ${Number(product.price).toLocaleString('en', {
-                            style: 'decimal',
-                            minimumFractionDigits: 2
-                        })}
-                    </td>
-                    <td
-                        className="text-right"
-                    >
-                        <Button
-                            bsStyle="primary"
-                            className={styles.button}
-                            disabled={product.quantityInCart >= product.quantityOnHand}
-                            onClick={() => adjustQuantity({
-                                id: product.id,
-                                quantity: 1
-                            })}
+    <div>
+        {
+            products.length === 0 &&
+                <Alert
+                    bsSize="small"
+                    bsStyle="info"
+                >
+                    No items available for purchase
+                </Alert>
+        }
+
+        <Table
+            bordered
+            hover
+            striped
+            verticalAlign="middle"
+        >
+            <tbody>
+                {products.map((product, i) => (
+                    <tr key={i}>
+                        <td>
+                            {product.id}
+                        </td>
+                        <td
+                            width="67%"
                         >
-                            Add to cart
-                        </Button>
-                        <Button
-                            bsStyle="danger"
-                            className={styles.button}
-                            onClick={() => removeItem({
-                                id: product.id
+                            {product.name}
+                        </td>
+                        <td>
+                            ${Number(product.price).toLocaleString('en', {
+                                style: 'decimal',
+                                minimumFractionDigits: 2
                             })}
+                        </td>
+                        <td
+                            className="text-right"
                         >
-                            Remove
-                        </Button>
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-    </Table>
+                            <Button
+                                bsStyle="primary"
+                                className={styles.button}
+                                disabled={product.quantityInCart >= product.quantityOnHand}
+                                onClick={() => adjustQuantity({
+                                    id: product.id,
+                                    quantity: 1
+                                })}
+                            >
+                                Add to cart
+                            </Button>
+                            <Button
+                                bsStyle="danger"
+                                className={styles.button}
+                                onClick={() => removeItem({
+                                    id: product.id
+                                })}
+                            >
+                                Remove
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    </div>
 );
