2a9ad2c7ab5879e08fada3ef88d022441c9b9f2d
Chris Wright <chris@arcane.ws>
2017-01-11 22:24:58 -0500

    Changed cart reducer now accepts update quantity action

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,8 +1,26 @@
+import {
+    UPDATE_QUANTITY
+} from '../actions/cart';
+
 const initialState = {
 };
 
 export default (state = initialState, action = {}) => {
     switch (action.type) {
+        case UPDATE_QUANTITY:
+            if (action.quantity <= 0) {
+                const {
+                    [action.id.toString()]: removedItem,
+                    ...newState
+                } = state;
+
+                return newState;
+            }
+
+            return {
+                ...state,
+                [action.id]: parseInt(action.quantity, 10)
+            };
         default:
             return state;
     }
