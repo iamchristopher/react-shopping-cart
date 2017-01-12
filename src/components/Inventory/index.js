02c72770c916709c34feb9af6dd549e8b51f2550
Chris Wright <chris@arcane.ws>
2017-01-11 20:31:42 -0500

    Changed inventory list is now a table

--- src/components/Inventory/index.js
+++ src/components/Inventory/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import {
-    ListGroup,
-    ListGroupItem
+    Button,
+    Table
 } from 'react-bootstrap';
 
 export default ({ products }) => (
-    <div>
-        <h2>Inventory</h2>
-        <ListGroup>
+    <Table
+        bordered
+        hover
+        striped
+    >
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
             {products.map((product, i) => (
-                <ListGroupItem key={i}>
-                    {product.name}
-                </ListGroupItem>
+                <tr key={i}>
+                    <td>
+                        {product.name}
+                    </td>
+                    <td>
+                        ${Number(product.price).toLocaleString('en', {
+                            style: 'decimal',
+                            minimumFractionDigits: 2
+                        })}
+                    </td>
+                    <td>
+                        <Button
+                            bsStyle="primary"
+                        >
+                            Add to cart
+                        </Button>
+                    </td>
+                </tr>
             ))}
-        </ListGroup>
-    </div>
+        </tbody>
+    </Table>
 );
