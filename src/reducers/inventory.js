c74b9c15a2f2c4ec7c0ccca08e085aa7d17e114e
Chris Wright <chris@arcane.ws>
2017-01-11 20:05:53 -0500

    Added prices to products

--- src/reducers/inventory.js
+++ src/reducers/inventory.js
@@ -1,7 +1,22 @@
 const initialState = {
-    1: { name: 'Item 1', quantityOnHand: 1 },
-    2: { name: 'Item 2', quantityOnHand: 10 },
-    3: { name: 'Item 3', quantityOnHand: 12 }
+    1: {
+        id: 1,
+        name: 'Item 1',
+        price: 1.5,
+        quantityOnHand: 1
+    },
+    2: {
+        id: 2,
+        name: 'Item 2',
+        price: 1.5,
+        quantityOnHand: 10
+    },
+    3: {
+        id: 3,
+        name: 'Item 3',
+        price: 1.5,
+        quantityOnHand: 12
+    }
 };
 
 export default (state = initialState, action = {}) => {
